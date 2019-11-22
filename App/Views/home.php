b927e7eabb8219b18e862797f1f494b3639a19b9
brunoorosco <40275604+brunoorosco@users.noreply.github.com>
2019-11-22 09:00:05 -0300

    inicio

---
+++ App/Views/home.php
@@ -0,0 +1,13 @@
+<section id="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Vagas em aberto</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+         
+        </div>
+    </div>
+</section>
